6f5d44a5276652a4344c54607680d6672f71cc95
hackerhalt <hackerhalt02@gmail.com>
2025-10-15 13:12:20 +0530

    update the main.sh

--- main.sh
+++ main.sh
@@ -10,6 +10,7 @@ show_menu() {
   echo "6) Exit"
 }
 
+# Enumeration
 run_enumeration() {
   echo "Select Enumeration Script:"
   echo "1) SMB Enum"
@@ -24,6 +25,67 @@ run_enumeration() {
   esac
 }
 
+# Exploitation
+run_Exploitation() {
+  echo "Select Enumeration Script:"
+  echo "1) SMB Enum"
+  echo "2) HTTP Enum"
+  echo "3) FTP Enum"
+  read -p "Choice: " enum_choice
+  case $enum_choice in
+    1) bash ./Enumeration/smb-enum.sh ;;
+    2) bash ./Enumeration/http-enum.sh ;;
+    3) bash ./Enumeration/ftp-enum.sh ;;
+    *) echo "Invalid choice." ;;
+  esac
+}
+
+# Networking
+run_Networking() {
+  echo "Select Enumeration Script:"
+  echo "1) SMB Enum"
+  echo "2) HTTP Enum"
+  echo "3) FTP Enum"
+  read -p "Choice: " enum_choice
+  case $enum_choice in
+    1) bash ./Enumeration/smb-enum.sh ;;
+    2) bash ./Enumeration/http-enum.sh ;;
+    3) bash ./Enumeration/ftp-enum.sh ;;
+    *) echo "Invalid choice." ;;
+  esac
+}
+
+# Information-Gathering 
+run_Information-Gathering() {
+  echo "Select Enumeration Script:"
+  echo "1) SMB Enum"
+  echo "2) HTTP Enum"
+  echo "3) FTP Enum"
+  read -p "Choice: " enum_choice
+  case $enum_choice in
+    1) bash ./Enumeration/smb-enum.sh ;;
+    2) bash ./Enumeration/http-enum.sh ;;
+    3) bash ./Enumeration/ftp-enum.sh ;;
+    *) echo "Invalid choice." ;;
+  esac
+}
+
+# Reporting 
+run_Reporting() {
+  echo "Select Enumeration Script:"
+  echo "1) SMB Enum"
+  echo "2) HTTP Enum"
+  echo "3) FTP Enum"
+  read -p "Choice: " enum_choice
+  case $enum_choice in
+    1) bash ./Enumeration/smb-enum.sh ;;
+    2) bash ./Enumeration/http-enum.sh ;;
+    3) bash ./Enumeration/ftp-enum.sh ;;
+    *) echo "Invalid choice." ;;
+  esac
+}
+
+
 # Add similar functions for other categories...
 
 while true; do
@@ -31,8 +93,8 @@ while true; do
   read -p "Enter choice: " main_choice
   case $main_choice in
     1) run_enumeration ;;
-    2) echo "Exploitation not added yet" ;; # Add later
-    3) echo "Network Scanning not added yet" ;; # Add later
+    2) run_Exploitation ;; # Add later
+    3) run_Networking ;; # Add later
     4) echo "Info Gathering not added yet" ;; # Add later
     5) echo "Reporting not added yet" ;; # Add later
     6) exit 0 ;;
